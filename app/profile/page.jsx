8ad8cb5ca899d9808b2a5f09c41328066df74e55
Jayesh Wadhe <jwadhe46@gmail.com>
2023-07-07 15:48:41 +0530

    loader

---
+++ app/profile/page.jsx
@@ -0,0 +1,14 @@
+'use client';
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import PromptCard from "@components/PromptCard";
+
+const MyProfile = () => {
+  return (
+    <div>Myprofile</div>
+  )
+}
+
+export default MyProfile
