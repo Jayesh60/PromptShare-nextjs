d293ddee9b02f1dafd5586570ef1ae501c13ad36
Jayesh Wadhe <jwadhe46@gmail.com>
2023-08-01 21:02:00 +0530

    missing schema bug reported * 2

--- app/api/create-prompt/route.js
+++ app/api/create-prompt/route.js
@@ -1,14 +1,15 @@
 import { connectToDb  } from "@utils/database";
-import Prompt from "@models/prompt.js";
+import Prompt from "@models/prompt";
+import User from "@models/user"
 
 // export const runtime = "edge"
 
-export const GET = async (request) =>{
+export const GET = async () =>{
     try {
         await connectToDb();
         const prompts = await Prompt.find({}).sort([["_id", -1]]).populate('creator');
         return new Response(JSON.stringify(prompts),{status:200});
-         
+
     } catch (error) {
         console.log(error + "prompts failed to load feed")
         return new Response(JSON.stringify("failed"),{status:500});
