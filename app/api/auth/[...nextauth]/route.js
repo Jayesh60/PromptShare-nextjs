bc564ec051c2057805e676299464bba81cdce910
Jayesh Wadhe <jwadhe46@gmail.com>
2023-08-08 19:17:19 +0530

    change

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -22,7 +22,7 @@ const handler = NextAuth({
             const sessionUser = await User.findOne({
                 email: session.user.email
             })
-            session.user.id = await sessionUser._id.toString()
+            session.user.id = sessionUser._id.toString()
             return session;
         },
         async signIn({
